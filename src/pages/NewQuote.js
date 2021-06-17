30285eec5b0335df0dc1879a09b621a7cf7300cd
Veronika Ozretic <vozret00@fesb.hr>
2021-06-17 19:00:41 +0200

    programmatic (imperative) navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,19 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  // use History -> a hook which changes the history of pages
+  // returns an object
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
+    // if we send our data here, we eill be directed back to the "quotes" page
     console.log(quoteData);
+
+    // history.push -> pushes a new page on the stack of pages, has a "back" button
+    // history.replace -> replaces a certain page, can't go back, redirect
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
